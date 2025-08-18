41fb7cf03c4b6092c0f509a1b7d06c5990eb0add
KNTy1018 <soramametaro1018@gmail.com>
2025-08-19 00:01:59 +0900

    [add]アニメーションでセレクトを制御している途中

--- Assets/Script/GameSelectButton.cs
+++ Assets/Script/GameSelectButton.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class GameSelectButton : MonoBehaviour
 {
     [SerializeField] List<GameObject> _gameSelectList;
-    [SerializeField] Vector3 _defaultPos;
-    [SerializeField] float _space = 650;
     [SerializeField] List<Animator> _animators;
+    [SerializeField] Vector3 _defaultPos;
+    [SerializeField] Vector3 _defaultScale;
+    [SerializeField] float _space = 650;
+    [SerializeField] float _maxScale = 1f;
+    [SerializeField] float _minScale = 0.8f;
 
     int _selectIndex;
     int _nowSelectIndex = 0;
+    bool _isChanging = false;
 
     private void Start()
     {
@@ -23,33 +28,105 @@ public class GameSelectButton : MonoBehaviour
         foreach (var gameSelect in _gameSelectList)
         {
             gameSelect.transform.localPosition = _defaultPos + new Vector3(_space * count, 0, 0);
+            if (count != 0)
+            {
+                gameSelect.transform.localScale = _defaultScale * _minScale;
+            }
             count++;
         }
     }
 
     public void RightChange()
     {
-        if (_nowSelectIndex < _selectIndex - 1)
+        if (!_isChanging)
         {
-            for (int i = 0; i < _selectIndex; i++)
+            if (_nowSelectIndex < _selectIndex - 1)
             {
-                _gameSelectList[i].transform.localPosition = new Vector3(_gameSelectList[i].transform.localPosition.x, _defaultPos.y, _gameSelectList[i].transform.localPosition.z);
-                _animators[i].Play("GameSelectLeft");
+                for (int i = 0; i < _selectIndex; i++)
+                {
+                    _gameSelectList[i].transform.localPosition = new Vector3(_gameSelectList[i].transform.localPosition.x, _defaultPos.y, _gameSelectList[i].transform.localPosition.z);
+
+                    if (i == _nowSelectIndex)
+                    {
+                        Debug.Log("ScaleDown");
+                        _animators[i].Play("ScaleDownL");
+                        StartCoroutine(ScaleDownCoroutine(_gameSelectList[i]));
+                    }
+                    else if (i == _nowSelectIndex + 1)
+                    {
+                        Debug.Log("ScaleUp");
+                        _animators[i].Play("ScaleUpL");
+                        StartCoroutine(ScaleUpCoroutine(_gameSelectList[i]));
+                    }
+                    else
+                    {
+                        _animators[i].Play("GameSelectLeft");
+                    }
+                }
+                _nowSelectIndex++;
             }
-            _nowSelectIndex++;
         }
     }
 
     public void LeftChange()
     {
-        if (_nowSelectIndex > 0)
+        if (!_isChanging)
         {
-            for (int i = 0; i < _selectIndex; i++)
+            if (_nowSelectIndex > 0)
             {
-                _gameSelectList[i].transform.localPosition = new Vector3(_gameSelectList[i].transform.localPosition.x, _defaultPos.y, _gameSelectList[i].transform.localPosition.z);
-                _animators[i].Play("GameSelectRight");
+                for (int i = 0; i < _selectIndex; i++)
+                {
+                    _gameSelectList[i].transform.localPosition = new Vector3(_gameSelectList[i].transform.localPosition.x, _defaultPos.y, _gameSelectList[i].transform.localPosition.z);
+
+                    if (i == _nowSelectIndex)
+                    {
+                        Debug.Log("ScaleDown");
+                        _animators[i].Play("ScaleDownR");
+                        StartCoroutine(ScaleDownCoroutine(_gameSelectList[i]));
+                    }
+                    else if (i == _nowSelectIndex - 1)
+                    {
+                        Debug.Log("ScaleUp");
+                        _animators[i].Play("ScaleUpR");
+                        StartCoroutine(ScaleUpCoroutine(_gameSelectList[i]));
+                    }
+                    else
+                    {
+                        _animators[i].Play("GameSelectRight");
+                    }
+                }
+                _nowSelectIndex--;
             }
-            _nowSelectIndex--;
         }
+    }
+
+    IEnumerator ScaleUpCoroutine(GameObject obj)
+    {
+        yield return new WaitForSeconds(0.9f);
+        obj.transform.localScale = _defaultScale * _maxScale;
+        yield break;
+    }
+
+    IEnumerator ScaleDownCoroutine(GameObject obj)
+    {
+        yield return new WaitForSeconds(0.9f);
+        obj.transform.localScale = _defaultScale * _minScale;
+        yield break;
+    }
+
+    public void Changing()
+    {
+        if (!_isChanging)
+        {
+            StartCoroutine(ChangingCoroutine());
+        }
+    }
+
+    IEnumerator ChangingCoroutine()
+    {
+        _isChanging = true;
+        yield return new WaitForSeconds(0.9f);
+        _isChanging = false;
+        yield break;
     }
 }
