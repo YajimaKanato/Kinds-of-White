7d405eef38bf0ab7b8da4e800ee9e242b80ec3ae
KNTy1018 <soramametaro1018@gmail.com>
2025-08-20 21:52:08 +0900

    [add]説明に動きを追加

---
+++ Assets/Script/Info.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+
+public class Info : MonoBehaviour
+{
+    Coroutine _openCoroutine;
+    Coroutine _closeCoroutine;
+
+    Vector3 _defaultScale;
+
+    float _maxScale = 1.0f;
+    float _minScale = 0.1f;
+
+    const float SCALETIME = 0.5f;
+
+    private void Start()
+    {
+        _defaultScale = transform.localScale;
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if (_openCoroutine == null)
+        {
+            _openCoroutine = StartCoroutine(OpenCoroutine());
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_openCoroutine != null)
+        {
+            StopCoroutine(_openCoroutine);
+            _openCoroutine = null;
+        }
+    }
+
+    public void Close()
+    {
+        if (_closeCoroutine != null)
+        {
+            StopCoroutine(_closeCoroutine);
+            _closeCoroutine = null;
+        }
+        _closeCoroutine = StartCoroutine(CloseCoroutine());
+    }
+
+    IEnumerator OpenCoroutine()
+    {
+        float delta = 0;
+        float scaleDelta = _maxScale - _minScale;
+        while (true)
+        {
+            delta += Time.deltaTime;
+            transform.localScale = _defaultScale * (_minScale + scaleDelta * delta / SCALETIME);
+            if (delta > SCALETIME)
+            {
+                transform.localScale = _defaultScale * _maxScale;
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator CloseCoroutine()
+    {
+        float delta = 0;
+        float scaleDelta = _minScale - _maxScale;
+        while (true)
+        {
+            delta += Time.deltaTime;
+            transform.localScale = _defaultScale * (_maxScale + scaleDelta * delta / SCALETIME);
+            if (delta > SCALETIME)
+            {
+                transform.localScale = _defaultScale * _minScale;
+                gameObject.SetActive(false);
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
